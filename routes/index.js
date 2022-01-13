0c9456b876668093e64bf61a62e6b4ef27befc8e
James Spencer <jameskcs1254@gmail.com>
2022-01-13 12:44:35 +0000

    Move messages to new post module

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var newMessage = require('./new')
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+const messages = newMessage.messages;
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini-Message Board', messages: messages });
 });
